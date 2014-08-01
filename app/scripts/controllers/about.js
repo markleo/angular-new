5a50257eaa5738d256d473d48b325c8368321ad5
Bunleo <n.khanhhoang@gmail.com>
2014-08-01 16:13:17 +0700

    init

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularNewsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularNewsApp
+ */
+angular.module('angularNewsApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
