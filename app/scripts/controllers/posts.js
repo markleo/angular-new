6ce6fd0942f88ba095b3e3468a50b27c759486ef
hoangnguyen <n.khanhhoang@gmail.com>
2014-08-04 10:29:08 +0700

    issue3 fixed

--- app/scripts/controllers/posts.js
+++ app/scripts/controllers/posts.js
@@ -1,17 +1,28 @@
 (function(){
 	'use strict';
-	app.controller('PostsCtrl', function($scope){
-		$scope.posts = [];
+	app.controller('PostsCtrl', function($scope, $location, Post) {
+		$scope.posts = Post.all;
 		$scope.post = {url: 'http://', title: ''};
 
 		$scope.addPost = function()  {
-			$scope.posts.push($scope.post);
-			$scope.post = {url: 'http://', title: ''};
+			// using method of firebase
+			Post.create($scope.post).then(function(ref) {
+				$location.path('/post/' + ref.name());
+				$scope.post = {url: 'http://', 'title': ''};
+			});
+			// method of $reso
+			// Post.save($scope.post, function (ref) {
+			//   $scope.posts[ref.name] = $scope.post;
+			//   $scope.post = {url: 'http://', title: ''};
+			// });
 		};
 
 		// delete Post
-		$scope.delPost = function (index) {
-			$scope.posts.splice(index, 1);
-		}
+		$scope.delPost = function (postId) {
+			Post.delete(postId);
+			// Post.delete({id: postId}, function () {
+		 //    	delete $scope.posts[postId];
+		 //  	});
+		};
 	});
 })();
